b0db4f10477720e07a932edfc35165f9049fd878
TK <k@tks-macbook-pro.local>
2017-08-18 19:25:01 +0800

    Initial Commit

---
+++ zzz/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  zzz
+//
+//  Created by TK on 18/08/2017.
+//  Copyright © 2017 TK. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ zzz/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  zzz
+//
+//  Created by TK on 18/08/2017.
+//  Copyright © 2017 TK. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
